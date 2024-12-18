5f2974393c81a67a8a18ea4a69716cbb84c1d674
basita512 <mail2basita@gmail.com>
2024-12-18 22:29:28 +0530

    added middleare and auth routes

--- Backend/Routes/user.js
+++ Backend/Routes/user.js
@@ -2,13 +2,14 @@ const express = require('express')
 const router = express.Router()
 const jwt = require('jsonwebtoken')
 const zod = require('zod')
-const user = require('../Database/db')
+const { User } = require('../Database/db')
 const {JWT_SECRET} = require('../config')
 
 const app = express()
 app.use(express.json())
 const schema = zod.array(zod.number())
 
+// When user Sign-up
 const signUpBody = zod.object({
     email : zod.string().email(),
     firstName : zod.string(),
@@ -16,9 +17,54 @@ const signUpBody = zod.object({
     password : zod.string().min(8),
 })
 
+app.post('/sign-up', async(req, res) => {
+    const response = signUpBody.safeParse(req.body)
+
+    // Wrong Inputs check
+    if(!response.success) {
+        console.log(response)
+        return res.status(411).json({
+            message: 'Incorrect Inputs'
+        })
+    }
+
+    // Existing User check
+    const existingUser = await User.findOne({
+        username : req.body.username
+    })
+
+    if (existingUser) {
+        return res.status(411).json({
+            message : 'User belonging to this Email ID already exists'
+        })
+    }
+
+    //If new User
+    const user = await User.create({
+        username : req.body.username,
+        password : req.body.password,
+        firstName : req.body.firstName,
+        lastName : req.body.lastName
+    })
+    const userId = user._id
+
+    const token = jwt.sign({userId}, JWT_SECRET)
+    res.json({
+        message: 'User created successfully',
+        token : token
+    })
+})
+
+
+// When user sign-in
+const signInBody = zod.object({
+    username : req.body.username,
+    password : req.body.password
+})
+
 app.post('/sign-in', async(req, res) => {
-    const userLogin = req.body.userLogin
-    const response = signUpBody.safeParse(userLogin)
+    //const userLogin = req.body.userLogin
+    const response = signInBody.safeParse(userLogin)
 
     if(!response.success) {
         console.log(response)
@@ -27,16 +73,25 @@ app.post('/sign-in', async(req, res) => {
         })
     }
     
-    const user = await userLogin.findOne({
+    const user = await User.findOne({
         username : req.body.username,
         password : req.body.password
     })
+    const userId = user._id
 
+    // If user exists
     if (user) {
-        const token = jwt.sign({
-
+        const token = jwt.sign({userId}, JWT_SECRET)
+        res.json({
+            token : token
         })
+        return
     }
+
+    res.status(411).json({
+        message: 'Error while logging in'
+    })
+
 })
 
 module.exports = router
---
+++ Backend/middleware.js
@@ -0,0 +1,30 @@
+const {JWT_SECRET} = require('./config')
+const jwt = require('jsonwebtoken')
+
+const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization
+
+    //Condition if the header doesn't starts with 'Bearer'
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status.json({
+            error: 'Invalid Token'
+        })
+    }
+
+    //split(' ') splits the string at spaces, resulting in an array: ['Bearer', '<token>'].
+    //[1] gets the second element (the actual token).
+    const token = authHeader.split(' ')[1]
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET)
+        req.userId = decoded.userId
+        next()
+
+    } catch (error) {
+        return res.status(403).json({})
+    }
+}
+
+module.exports = {
+    authMiddleware
+}
